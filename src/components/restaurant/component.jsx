96fb522e737e1843dc58d2e1047c6fb3f3a0e897
Eric Wegscheid <ericwegscheid@gmail.com>
2019-06-18 01:27:11 -0500

    feat: add price and open status

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -12,11 +12,14 @@ export class Restaurant extends Component {
   }
 
   render() {
-    const { image_url, name } = this.props.restaurant
+    const { image_url, is_closed, price, name } = this.props.restaurant
 
     return <div className="restaurant">
       <div className="thumb" style={{ backgroundImage: `url(${image_url})` }}></div>
       <h4>{name}</h4>
+      <span className="category">Category</span>
+      <span className="price">{price}</span>
+      <span className={`isOpen ${is_closed ? 'closed' : ''}`}>{is_closed ? 'Closed' : 'Open Now'}</span>
       <Button
         onClick={this.onClickLearnMore.bind(this)}
       >
