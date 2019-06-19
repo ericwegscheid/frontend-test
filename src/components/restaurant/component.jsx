99b88449712160212afb4fd2698b0ac7b20e3284
Eric Wegscheid <ericwegscheid@gmail.com>
2019-06-18 20:03:21 -0500

    fix: case when images for restaurant are not available

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -15,11 +15,16 @@ export class Restaurant extends Component {
     const { image_url, is_closed, price, name } = this.props.restaurant
 
     return <div className="restaurant">
-      <div className="thumb" style={{ backgroundImage: `url(${image_url})` }}></div>
+      <div
+        className={`thumb ${image_url ? '' : 'default'}`}
+        style={{ backgroundImage: `url(${image_url})` }}
+      ></div>
       <h4>{name}</h4>
       <span className="category">Category</span>
       <span className="price">{price}</span>
-      <span className={`isOpen ${is_closed ? 'closed' : ''}`}>{is_closed ? 'Closed' : 'Open Now'}</span>
+      <span className={`isOpen ${is_closed ? 'closed' : ''}`}>
+        {is_closed ? 'Closed' : 'Open Now'}
+      </span>
       <Button
         onClick={this.onClickLearnMore.bind(this)}
       >
