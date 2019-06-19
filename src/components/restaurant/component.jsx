258dd727417dabdfe46de28f12aae1999ed75bb2
Eric Wegscheid <ericwegscheid@gmail.com>
2019-06-18 21:17:36 -0500

    feat: add mixins for common shapes, style restaurant search details info

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -11,6 +11,10 @@ export class Restaurant extends Component {
     console.log('learn more')
   }
 
+  getCategory() {
+    return 'Category'
+  }
+
   render() {
     const { image_url, is_closed, price, name } = this.props.restaurant
 
@@ -20,11 +24,15 @@ export class Restaurant extends Component {
         style={{ backgroundImage: `url(${image_url})` }}
       ></div>
       <h4>{name}</h4>
-      <span className="category">Category</span>
-      <span className="price">{price}</span>
-      <span className={`isOpen ${is_closed ? 'closed' : ''}`}>
-        {is_closed ? 'Closed' : 'Open Now'}
-      </span>
+      <div>star star star star star</div>
+      <div className="details">
+        <span className="category-price">
+          {this.getCategory()} &bull; {price}
+        </span>
+        <span className={`is-open ${is_closed ? 'closed' : ''}`}>
+          {is_closed ? 'Closed' : 'Open Now'}
+        </span>
+      </div>
       <Button
         onClick={this.onClickLearnMore.bind(this)}
       >
