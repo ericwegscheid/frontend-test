67b1d9c969c3f27cc349bdf6d4884e9574989453
Eric Wegscheid <ericwegscheid@gmail.com>
2019-06-19 21:13:42 -0500

    chore: update README.md with specifics about installing and running the project

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # Superformula Front-end Developer Coding Test
 
-Do not fret! Details are coming.
+This project leverages Yelp's Fusion API to search for restaurants in the Las Vegas area by the top 10 most popular restaurant categories, price, and open status, as well as displays restaurant details and reviews.
+
+## Installing / Getting started
+
+### Requirements
+
+* Yelp API Key
+* node 11+
+* npm 6+
+
+### Attain Yelp API Key
+
+To get this project up and running you must have an API Key. An API Key can be attained by [creating an app](https://www.yelp.com/developers/v3/manage_app) at [Yelp's developer website](https://www.yelp.com/developers/documentation/v3). You will be prompted to register an account or login to an existing account. Once you are logged in you should be able to reteive your API Key on the '[Manage App](https://www.yelp.com/developers/v3/manage_app)' page.
+
+
+### Clone Repository and Install Dependencies
+
+Paste the command below in your terminal. It will clone the repository and install its dependencies.
+
+```shell
+git clone https://github.com/ericwegscheid/frontend-test && cd frontend-test && npm i
+```
+
+### Build and Run Application
+
+> PRO TIP: please kill any process that may already be running on port 9000 prior to running.
+
+```shell
+npm run dev
+```
+
+Upon running for the first time you will be prompted to input your API Key in the terminal. Simply paste you API Key in the prompt and press enter.
+
+Alternatively, you may create a `.token` file in the root directory of the project with only your API Key in it prior to running.
+
+```shell
+echo 'YOUR_API_KEY' >> .token && npm run dev
+```
+
+After running the command above your default web browser should automatically open at `http://localhost:9000/`.
+
+You're all set!
+
+### For Production
+
+Create dist folder of bundled project in the project's root directory, which can used in a production environment.
+
+```shell
+npm run build
+```
+
+## Developer's notes
+
+...
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,96 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const fs = require('fs');
-const promptSync = require('prompt-sync')();
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fs = require('fs')
+const promptSync = require('prompt-sync')()
 
-let token;
+let token
 
-// check if API key exists
-if (fs.existsSync('.token')) {
-  token = fs.readFileSync('.token', 'utf8').trim();
+const setToken = () => {
+  // check if API key exists
+  if (fs.existsSync('.token')) {
+    token = fs.readFileSync('.token', 'utf8').trim()
+  }
+
+  // prompt for API key
+  if (!token) {
+    console.log([
+      '\n*** Missing API key ***\n',
+      'This application leverages Yelp\'s open API and requires a valid API key to function.\n', 
+      'For more info visit https://www.yelp.com/developers/documentation/v3\n',
+    ].join(''))
+    token = promptSync.hide('Please provide valid Yelp API key: ')
+    fs.writeFileSync('.token', token)
+  }
 }
 
-// prompt for API key
-if (!token) {
-  console.log([
-    '\n*** Missing API key ***\n',
-    'This application leverages Yelp\'s open API and requires a valid API key to function.\n', 
-    'For more info visit https://www.yelp.com/developers/documentation/v3\n',
-  ].join(''));
-  token = promptSync.hide('Please provide valid Yelp API key: ');
-  fs.writeFileSync('.token', token);
-}
+module.exports = env => {
+  if (env === 'development') {
+    setToken()
+  }
 
-module.exports = {
-  entry: ['babel-polyfill', './src/index.jsx'],
-  mode: 'development',
-  resolve: {
-    extensions: ['.js', '.jsx', '.less']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: false,
-    port: 9000,
-    proxy: {
-      '/api': {
-        target: 'https://api.yelp.com',
-        secure: true,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        },
-        headers: {
-          'Authorization': 'Bearer ' + token,
+  return {
+    entry: ['babel-polyfill', './src/index.jsx'],
+    mode: 'development',
+    resolve: {
+      extensions: ['.js', '.jsx', '.less']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: false,
+      port: 9000,
+      proxy: {
+        '/api': {
+          target: 'https://api.yelp.com',
+          secure: true,
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          },
+          headers: {
+            'Authorization': 'Bearer ' + token,
+          }
         }
       }
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', { useBuiltIns: "entry" }],
-                ['@babel/preset-react']
-              ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { useBuiltIns: "entry" }],
+                  ['@babel/preset-react']
+                ]
+              }
+            },
+            {
+              loader: 'eslint-loader',
             }
-          },
-          {
-            loader: 'eslint-loader',
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "less-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-};
+          ]
+        },
+        {
+          test: /\.less$/,
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "less-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ],
+  }
+}
