5f1fdd01eae96c2c542d84f2c28052a01166437f
Eric Wegscheid <ericwegscheid@gmail.com>
2019-06-23 23:56:48 -0500

    feat: begin implementing restaurant details component

--- src/components/restaurant-details/component.jsx
+++ src/components/restaurant-details/component.jsx
@@ -1,13 +1,44 @@
+import { get } from 'lodash'
 import React, { Component } from 'react'
+import { Oops, Row, Spinner, StarRating } from '../shared'
+import './styles'
 
 export class RestaurantDetailsComponent extends Component {
   constructor(props) {
     super(props)
+
+    this.props.fetchRestaurantDetails(props.id)
   }
 
   render() {
-    return <div>
-      <p>details</p>
+    const { category, error, is_closed, location, name, price, rating } = this.props
+    const photos = get(this.props, 'restaurantDetails.photos', [])
+
+    return <div className="restaurant-details">
+      <Row>
+        <h1 className="title">{name}</h1>
+        <StarRating rating={rating} />
+        <div className="details">
+          <span className="category-price">
+            {category} &bull; {price}
+          </span>
+          <span className={`is-open ${is_closed ? 'closed' : ''}`}>
+            {is_closed ? 'Closed' : 'Open Now'}
+          </span>
+        </div>
+      </Row>
+      <Row className="borders">
+        <p>{get(location, 'display_address', []).join(' ')}</p>
+      </Row>
+      {
+        this.props.isFetchingRestaurantDetails ?
+          <Spinner /> :
+          error ?
+            <Oops /> :
+            <Row>
+              <p>get the reviews</p>
+            </Row>
+      }
     </div>
   }
 }
--- src/components/restaurant-details/provider.js
+++ src/components/restaurant-details/provider.js
@@ -1,8 +1,16 @@
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchRestaurantDetails } from '../../store'
 import { RestaurantDetailsComponent } from './component'
 
 const mapStateToProps = state => ({
-  ...state,
+  ...state.detailsView,
 })
 
-export const RestaurantDetails = connect(mapStateToProps)(RestaurantDetailsComponent)
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    fetchRestaurantDetails,
+  }, dispatch)
+}
+
+export const RestaurantDetails = connect(mapStateToProps, mapDispatchToProps)(RestaurantDetailsComponent)
